f3a23e7d082d1b6a4e666835acb5525d2abaa23d
Aditya Vikram KIrtania <108626210+Aditya792max@users.noreply.github.com>
2026-07-28 09:19:46 +0530

    Users

---
+++ Server/Models/UserModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      validate: [validator.isEmail, "Please provide a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password length should be greater than 6 characters"],
+      select: true,
+    },
+    location: {
+      type: String,
+      default: "India",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
